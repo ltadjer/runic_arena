ec6dd3a64184543db3307dd8b80e96409ccc11d2
ltadjer <94218573+ltadjer@users.noreply.github.com>
2024-10-16 14:47:43 +0200

    set a crud for cards

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -5,22 +5,75 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Card;
+use App\Form\CardType;
+use Symfony\Component\HttpFoundation\Request;
 class CardController extends AbstractController
 {
-    #[Route('/card', name: 'app_card')]
-    public function index(): Response
+    #[Route('/cartes', name: 'card_list')]
+    public function list(EntityManagerInterface $em): Response
     {
+        $cards = $em->getRepository(Card::class)->findAll();
         return $this->render('card/index.html.twig', [
-            'controller_name' => 'CardController',
+            'cards' => $cards,
         ]);
     }
 
-    #[Route('/cartes/ajouter', name: 'app_card_add')]
-    public function new(): Response
+    #[Route('/cartes/ajouter', name: 'card_add')]
+    public function add(Request $request, EntityManagerInterface $em): Response
+{
+    $card = new Card();
+    $card->setCreatedAt(new \DateTimeImmutable()); // Initialisation du champ createdAt
+    dump($this->getUser());
+    $card->setUser($this->getUser());
+    $form = $this->createForm(CardType::class, $card);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($card);
+        $em->flush();
+        return $this->redirectToRoute('card_list');
+    }
+
+    return $this->render('card/add.html.twig', [
+        'form' => $form->createView(), // Assurez-vous que le formulaire est rendu correctement
+    ]);
+}
+
+    #[Route('/cartes/editer/{id}', name: 'card_edit')]
+    public function editer($id, EntityManagerInterface $em, Request $request): Response
     {
-        return $this->render('card/add.html.twig', [
-            'controller_name' => 'CardController',
+        $card = $em->getRepository(Card::class)->find($id);
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('card_list');
+        }
+
+        return $this->render('card/edit.html.twig', [
+            'form' => $form,
+            'card' => $card
         ]);
     }
+
+    #[Route('/cartes/supprimer/{id}', name: 'card_delete')]
+    public function delete($id, EntityManagerInterface $em): Response
+    {
+        $card = $em->getRepository(Card::class)->find($id);
+        $em->remove($card);
+        $em->flush();
+        return $this->redirectToRoute('card_list');
+    }
+
+    #[Route('/cartes/{id}', name: 'card_show')]
+    public function show(Card $card): Response
+    {
+        return $this->render('card/show.html.twig', [
+            'card' => $card,
+        ]);
+    }
+
 }
---
+++ src/Form/CardType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Card;
+use App\Entity\classCard;
+use App\Entity\typeCard;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CardType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('attackPower')
+            ->add('type', EntityType::class, [
+                'class' => typeCard::class,
+                'choice_label' => 'name',
+            ])
+            ->add('class', EntityType::class, [
+                'class' => classCard::class,
+                'choice_label' => 'name',
+            ])
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Card::class,
+        ]);
+    }
+}
